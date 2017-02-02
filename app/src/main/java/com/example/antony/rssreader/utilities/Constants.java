ec4936a152cfebe79c864867a69d284eb0bba2e2
Anton Pripachkin <anton.pripachkin@extrawest.com>
2017-02-02 15:47:35 +0200

    added menu and constants

---
+++ app/src/main/java/com/example/antony/rssreader/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.example.antony.rssreader.utilities;
+
+/**
+ * Created by Antony on 2/2/2017.
+ */
+
+public class Constants {
+    public static final String KOTAKU_RSS_FEED_LINK = "http://feeds.gawker.com/kotaku/full";
+}
