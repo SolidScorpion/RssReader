51fd27affb2e478218c6f78acda3bf3e395e3bbf
Anton Pripachkin <anton.pripachkin@extrawest.com>
2017-02-02 18:00:16 +0200

    making backbone of the project

--- app/src/main/java/com/example/antony/rssreader/MainActivity.java
+++ app/src/main/java/com/example/antony/rssreader/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.antony.rssreader;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView mMenuRw;
+    private RecyclerView mMainContentRw;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init(savedInstanceState);
     }
+
+    private void init(Bundle savedInstanceState) {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mMenuRw = (RecyclerView) findViewById(R.id.menuRw);
+        mMainContentRw = (RecyclerView) findViewById(R.id.contentRw);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer_descr, R.string.app_name);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 }
